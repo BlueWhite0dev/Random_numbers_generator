3d063d0921554c55bab6de8861a733d44c5cb44e
BlueWhite0dev <BlueWhite.dev@gmail.com>
2024-05-06 22:09:33 +0200

    Created website

---
+++ script.js
@@ -0,0 +1,76 @@
+document.querySelector("input[type='submit']").addEventListener("click", checkOptions);
+document.querySelector(".copy").addEventListener("click", getCopy);
+function checkOptions() {
+  event.preventDefault();
+  numbers = [];
+  let minValue = parseInt(document.querySelector("input[name='min']").value);
+  let maxValue = parseInt(document.querySelector("input[name='max']").value);
+  let quantityValue = parseInt(document.querySelector("input[name='quantity']").value);
+  minValue < maxValue ? generateNumbers(minValue, maxValue, quantityValue) : showAlert("sequence");
+}
+
+function generateNumbers(min, max, quantity) {
+  document.querySelector("input[value='float']").checked ? (floatValue = parseInt(document.querySelector("input[name='float']").value)) : (floatValue = 0);
+  if (document.querySelector("input[name='unique']").checked) {
+    let maxQuantity;
+    maxQuantity = Math.round((max - min) / Math.pow(0.1, floatValue) + 1);
+    if (maxQuantity < quantity) {
+      quantity = maxQuantity;
+      document.querySelector("input[name='quantity']").value = maxQuantity;
+    }
+  }
+
+  let number = 0;
+  for (let i = 0; i < quantity; i++) {
+    number = (Math.random() * (max - min) + min).toFixed(floatValue);
+    if (number == -0) {
+      number = 0;
+    } else if (document.querySelector("input[value='float']").checked) {
+      number = parseFloat(number);
+    }
+    if (document.querySelector("input[name='unique']").checked) {
+      if (numbers.includes(number)) {
+        i--;
+      } else {
+        numbers[i] = number;
+      }
+    } else {
+      numbers[i] = number;
+    }
+  }
+  getNumbers();
+}
+
+function getNumbers() {
+  if (document.querySelector("input[name='sort']").checked) {
+    switch (document.querySelector("select[name='sort']").value) {
+      case "ascending":
+        numbers.sort((a, b) => a - b);
+        break;
+      case "descending":
+        numbers.sort((a, b) => b - a);
+        break;
+    }
+  }
+  showNumbers();
+}
+
+function showNumbers() {
+  let output = document.querySelector(".result textarea");
+  let separate = document.querySelector("input[name='separate']").value;
+  for (let n = 0; n < numbers.length; n++) {
+    n == 0 ? (output.value = `${numbers[n]}`) : (output.value += `${separate} ${numbers[n]}`);
+  }
+}
+
+function showAlert(info) {}
+
+function getCopy() {
+  event.preventDefault();
+  let text = document.querySelector(".result textarea");
+  if (text.value != "") {
+    text.select();
+    text.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(text.value);
+  }
+}
